ef1a39be47c219da05e32c0f66578613eb81161c
Joao Paulo <joaoded@hotmail.com>
2018-01-10 12:57:06 -0200

    Sending project

---
+++ js/chartMain.js
@@ -0,0 +1,31 @@
+const _CHART = document.getElementById("allCoinChart");
+
+let allCoinChart = new Chart(_CHART, {
+  type: 'line',
+  data: {
+    labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho"],
+    datasets: [
+      {
+        fill: false,
+        lineTension: 0.1,
+        borderColor: "rgb(98,155,190)",
+        borderCapSyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgb(98,155,190)",
+        pointBackgroundColor: "rgb(98,155,190)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 1,
+        pointHoverBackgroundColor: "rgb(98,155,190)",
+        pointHoverBorderColor: "rgb(98,155,190)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [40,59,80,81,56,55,75],
+      }
+    ]
+
+  }
+
+});
